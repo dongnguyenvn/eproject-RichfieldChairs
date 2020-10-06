e257f1b2ea384aba6794c8b65d0ee42453abd3e5
shortear <shortear@users.noreply.github.com>
2020-10-06 18:55:37 +0700

    search page

--- routers/main.js
+++ routers/main.js
@@ -35,6 +35,72 @@ router.get("/product",function (req,res) {
     res.render("all-products");
 });
 
+//product detail page
+
+router.get("/product-detail/:id",async function (req,res) {
+    let productId= req.params.id;
+    const sql_text = "SELECT * FROM T2005E_BCB_Categories;SELECT * FROM T2005E_BCB_Brand; " +
+        "SELECT * FROM T2005E_BCB_Products WHERE ID="+productId+"; SELECT * FROM T2005E_BCB_Review WHERE IdProduct="+productId;
+    let data = {
+        categories: [],
+        brands: [],
+        product: {},
+        reviews:[]
+    }
+    try {
+        const rows = await db.query(sql_text);
+        data.categories = rows.recordsets[0];
+        data.brands = rows.recordsets[1];
+        data.product = rows.recordsets[2].length>0?rows.recordsets[2][0]:{};
+        data.reviews = rows.recordsets[3];
+    }catch (e) {
+
+    }
+    res.render("product-detail",data);
+})
+
+//search page
+
+router.get("/tim-kiem",async function (req,res) {
+    let keyword = req.query.search;
+    let page = req.query.page !== undefined?req.query.page:1;
+    const limit = 6;
+    let sql_text = "SELECT * FROM T2005E_BCB_Categories;SELECT * FROM T2005E_BCB_Brand;" +
+        `SELECT a.* FROM T2005E_BCB_Products as a ` +
+        "LEFT JOIN T2005E_BCB_Categories as b ON b.ID = a.CategoryID " +
+        "LEFT JOIN T2005E_BCB_Brand as c ON c.ID = a.BrandID " +
+        `WHERE a.TenSP LIKE N'%${keyword}%' `+
+        `OR b.CategoryName LIKE N'%${keyword}%' `+
+        `OR c.BrandName LIKE N'%${keyword}%' `+
+        `ORDER BY a.ID DESC OFFSET ${(page-1)*limit} ROWS FETCH FIRST ${limit} ROWS ONLY;`+
+        `SELECT count(a.ID) as total FROM SanPham as a ` +
+        "LEFT JOIN DanhMuc as b ON b.ID = a.DanhMucID " +
+        "LEFT JOIN ThuongHieu as c ON c.ID = a.ThuongHieuID " +
+        `WHERE a.TenSP LIKE N'%${keyword}%' `+
+        `OR b.TenDanhMuc LIKE N'%${keyword}%' `+
+        `OR c.TenThuongHieu LIKE N'%${keyword}%';`;
+    let data = {
+        danhmucs: [],
+        thuonghieus: [],
+        sanphams: [],
+        page:parseInt(page),
+        keyword:keyword,
+        total:0,
+        pageNumber:1
+    }
+    try{
+        const rows = await db.query(sql_text);
+        data.danhmucs = rows.recordsets[0];
+        data.thuonghieus = rows.recordsets[1];
+        data.sanphams = rows.recordsets[2];
+        data.total =  rows.recordsets[3][0].total;
+        data.pageNumber = Math.ceil(data.total/limit);
+    }catch (e) {
+        //console.log(e.message);
+    }
+   res.render("search",data);
+})
+
 
 // add remove products
 
